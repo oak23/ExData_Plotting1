10b2611e9bbace2d3ceb312c33a5d3f79762b5ef
Oliver Kuhn <oak.kuhn@gmail.com>
2016-07-24 12:20:55 +0200

    uses mutate to add column for datetime

--- plot1.R
+++ plot1.R
@@ -1,15 +1,15 @@
 library(dplyr)
+library(lubridate)
 
-Sys.setlocale("LC_TIME", "English")
-setwd('C:/Users/oak/Downloads/BasePlotting/ExData_Plotting1/')
+#Sys.setlocale("LC_TIME", "English")
+#setwd('C:/Users/oak/Downloads/BasePlotting/ExData_Plotting1/')
 
 fileurl = file.path(getwd(),'household_power_consumption.txt')
 
-dat = read.csv2(fileurl, header=T, na.strings = '?', as.is = T, dec='.') %>%
-  filter(Date %in% c('1/2/2007','2/2/2007'))
-
-datetime = strptime(paste(dat$Date, dat$Time, sep=" "), "%d/%m/%Y %H:%M:%S")
-
+dat = 
+  read.csv2(fileurl, header=T, na.strings = '?', as.is = T, dec='.') %>%
+  filter(Date %in% c('1/2/2007','2/2/2007')) %>%
+  mutate(datetime = dmy_hms(with(dat, paste(Date,Time))))
 
 png('plot1.png',width=480,height=480)
 
--- plot2.R
+++ plot2.R
@@ -1,18 +1,16 @@
 library(dplyr)
+library(lubridate)
 
-Sys.setlocale("LC_TIME", "English")
-setwd('C:/Users/oak/Downloads/BasePlotting/ExData_Plotting1/')
+#Sys.setlocale("LC_TIME", "English")
+#setwd('C:/Users/oak/Downloads/BasePlotting/ExData_Plotting1/')
 
 fileurl = file.path(getwd(),'household_power_consumption.txt')
 
-dat = read.csv2(fileurl, header=T, na.strings = '?', as.is = T, dec='.') %>%
-  filter(Date %in% c('1/2/2007','2/2/2007'))
-
-datetime = strptime(paste(dat$Date, dat$Time, sep=" "), "%d/%m/%Y %H:%M:%S")
-
+dat = 
+  read.csv2(fileurl, header=T, na.strings = '?', as.is = T, dec='.') %>%
+  filter(Date %in% c('1/2/2007','2/2/2007')) %>%
+  mutate(datetime = dmy_hms(with(dat, paste(Date,Time))))
 
 png('plot2.png',width=480,height=480)
-
-plot(datetime, dat$Global_active_power, ylab='Global Active power (kilowatts)',type='l', xlab='')
-
+with(dat, plot(datetime, Global_active_power, ylab='Global Active power (kilowatts)',type='l', xlab=''))
 dev.off()
